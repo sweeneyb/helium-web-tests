e5a518cda232f7e223c4d9436e8f280557210c1b
sweeneyb <tab126@gmail.com>
2024-12-27 15:33:45 -0500

    firefox headless better represents what I see

--- first.py
+++ first.py
@@ -1,7 +1,7 @@
 from helium import *
 
-#start_chrome('https://github.com/sweeneyb?tab=repositories', headless=True)
-start_chrome('https://github.com/sweeneyb?tab=repositories')
+start_firefox('https://github.com/sweeneyb?tab=repositories', headless=True)
+# start_chrome('https://github.com/sweeneyb?tab=repositories')
 write('briansweeney.dev')
 press(ENTER)
 get_driver().save_screenshot(r'screenshot.png')
